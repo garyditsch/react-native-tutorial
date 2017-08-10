6cd847b7c2c5799a4f9885c58c22afdd1c15e0e6
Gary Ditsch <garyditsch@gmail.com>
2017-08-10 16:59:25 -0400

    initializing repo halfway through tutorial

---
+++ index.ios.js
@@ -0,0 +1,18 @@
+// index.ios.js place code here for ios
+
+import React from 'react';
+import { AppRegistry, View } from 'react-native';
+import Header from './src/components/header';
+import AlbumList from './src/components/AlbumList';
+
+
+const App = () => {
+    return(
+        <View>
+            <Header headerText={'Album'} />
+            <AlbumList />
+        </View>
+    );
+};
+
+AppRegistry.registerComponent('albums', () => App);

ea1258b23dca6a15a024300a8858bbb60fb6a55a
Gary Ditsch <garyditsch@gmail.com>
2017-08-11 09:05:11 -0400

    added functioning button with styles and link to amazon pages

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import { Text, View, Image } from 'react-native';
+import { Text, View, Image, Linking } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
+import Button from './Button';
 
 
 const AlbumDetail = ( { album }) => {
 
-    const { title, artist, thumbnail_image, image } = album;
+    const { title, artist, thumbnail_image, image, url } = album;
 
     return (
         <Card>
@@ -22,11 +23,16 @@ const AlbumDetail = ( { album }) => {
                     <Text>{artist}</Text>
                 </View>
             </CardSection>
+
             <CardSection>
                 <Image
                 style={styles.imageStyle}
                 source={{ uri: image }}
                 />
+            </CardSection>
+
+            <CardSection>
+                <Button onPress={() => Linking.openURL(url)} />
             </CardSection>
         </Card>
     );
